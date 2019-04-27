adbba87baf617432b278a7e9af78f4f68d9c6ce4
maqinchao <1547791192@qq.com>
2019-04-27 16:16:27 +0800

    update

---
+++ src/main/java/com/mqc/netty/client/TimeClientHandler.java
@@ -0,0 +1,7 @@
+package com.mqc.netty.client;
+
+public class TimeClientHandler {
+    public void connect(){
+
+    }
+}
--- src/main/java/com/mqc/netty/server/TimeServerHandler.java
+++ src/main/java/com/mqc/netty/server/TimeServerHandler.java
@@ -1,4 +1,4 @@
-package com.mqc.netty;
+package com.mqc.netty.server;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
@@ -7,33 +7,28 @@ import io.netty.channel.ChannelHandlerContext;
 
 import java.util.Date;
 
-/**
- * User: kangfoo-mac
- * Date: 14-7-20
- * Time: 下午2:55
- */
 public class TimeServerHandler extends ChannelHandlerAdapter {
-
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ByteBuf buf = (ByteBuf) msg;
-        byte[] req = new byte[buf.readableBytes()];
-        buf.readBytes(req);
-
-        String body = new String(req, "UTF-8");
-        System.out.println("The time server receive order : " + body);
-        String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body) ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
-        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ByteBuf buffer=(ByteBuf)msg;
+        byte[] req=new byte[buffer.readableBytes()];
+        buffer.readBytes(req);
+        String body=new String(req,"UTF-8");
+        System.out.printf("received:"+body);
+        String currentTime="QUERY TIME ORDER".equalsIgnoreCase(body)?new Date(System.currentTimeMillis()).toString():"BAD ORDER";
+        ByteBuf resp= Unpooled.copiedBuffer(currentTime.getBytes());
         ctx.write(resp);
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        super.channelReadComplete(ctx);
         ctx.flush();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-        ctx.close();
+        super.exceptionCaught(ctx, cause);
+        ctx.flush();
     }
 }
